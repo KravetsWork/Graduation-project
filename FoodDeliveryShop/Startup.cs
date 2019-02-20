b8ce6bc716c35e898173e290f7ae05d892fce08d
KravetsWork <workkravets@gmail.com>
2019-02-20 15:50:09 +0200

    Setup of class Startup and Program

--- FoodDeliveryShop/Startup.cs
+++ FoodDeliveryShop/Startup.cs
@@ -6,15 +6,25 @@ using FoodDeliveryShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FoodDeliveryShop
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration["Data:FoodDeliveryShopProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 
@@ -29,6 +39,7 @@ namespace FoodDeliveryShop
                     name: "default",
                     template: "{controller=Product}/{action=List}/{id?}");
             });
+            SeedData.EnsurePopulated(app);
         }
     }
 }
