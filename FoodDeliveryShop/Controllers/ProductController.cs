2de3a36469c6b5f27d90bc9cfdf8f0c7c60d18b8
KravetsWork <workkravets@gmail.com>
2019-02-20 15:18:48 +0200

    Added first controller

---
+++ FoodDeliveryShop/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodDeliveryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+    }
+}
