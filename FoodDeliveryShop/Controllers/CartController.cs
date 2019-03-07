a9960ff86fe7f11b8899a4fde8d3316d27c4cfc1
KravetsWork <workkravets@gmail.com>
2019-03-07 20:25:01 +0200

    Displaying the Contents of the Cart

--- FoodDeliveryShop/Controllers/CartController.cs
+++ FoodDeliveryShop/Controllers/CartController.cs
@@ -3,16 +3,28 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using FoodDeliveryShop.Infrastructure;
 using FoodDeliveryShop.Models;
+using FoodDeliveryShop.Models.ViewModels;
 
 namespace FoodDeliveryShop.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
+
         public CartController(IProductRepository repo)
         {
             repository = repo;
         }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
         public RedirectToActionResult AddToCart(int productId, string returnUrl)
         {
             Product product = repository.Products
@@ -25,6 +37,7 @@ namespace FoodDeliveryShop.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
         public RedirectToActionResult RemoveFromCart(int productId,
             string returnUrl)
         {
@@ -38,11 +51,13 @@ namespace FoodDeliveryShop.Controllers
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
         private Cart GetCart()
         {
             Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
             return cart;
         }
+
         private void SaveCart(Cart cart)
         {
             HttpContext.Session.SetJson("Cart", cart);
---
+++ FoodDeliveryShop/Models/ViewModels/CartIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FoodDeliveryShop.Models;
+
+namespace FoodDeliveryShop.Models.ViewModels
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
