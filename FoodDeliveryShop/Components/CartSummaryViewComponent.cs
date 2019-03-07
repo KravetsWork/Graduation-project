946e66a01e8722ef259eb293703fe8b8e5db85a8
KravetsWork <workkravets@gmail.com>
2019-03-07 20:44:13 +0200

    Creating the View Component Class and View

---
+++ FoodDeliveryShop/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using FoodDeliveryShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryShop.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
