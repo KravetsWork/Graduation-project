cbf407e74ec89b84dbf22e5b562c7f834862d47d
KravetsWork <workkravets@gmail.com>
2019-03-07 20:49:53 +0200

    Adding the Checkout Process

---
+++ FoodDeliveryShop/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using FoodDeliveryShop.Models;  
+using Microsoft.AspNetCore.Mvc;  
+  
+namespace FoodDeliveryShop.Controllers  
+{  
+    public class OrderController : Controller  
+    {  
+        public ViewResult Checkout() => View(new Order());  
+    }  
+} 
