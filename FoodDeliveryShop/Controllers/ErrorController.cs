863ac5545ba4ef57f48ad9822d9d0d5723f86ee7
KravetsWork <workkravets@gmail.com>
2019-03-08 17:28:21 +0200

    Creating the Error Controller and View

---
+++ FoodDeliveryShop/Controllers/ErrorController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryShop.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ViewResult Error() => View();
+    }
+}
